3c96b76e1ca73a98110a15261d8050a5cd8ccaa1
IgaliMarcell <i.marci158@gmail.com>
2025-10-18 14:43:32 +0200

    Added rviz config to launch with launcher file

--- launch/gps_path_visualizer.launch.py
+++ launch/gps_path_visualizer.launch.py
@@ -1,7 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
 
 def generate_launch_description():
+    rviz_config_file = os.path.join(
+        os.path.dirname(__file__),
+        'markers.rviz'
+    )
+    
     return LaunchDescription([
         Node(
             package='iga_iqp',
@@ -13,6 +20,11 @@ def generate_launch_description():
             package='iga_iqp',
             executable='path_visualizer_node',
             name='path_visualizer_node',
+            output='screen'
+        ),
+        # RViz2 indítása
+        ExecuteProcess(
+            cmd=['rviz2', '-d', rviz_config_file],
             output='screen'
         ),
     ])
